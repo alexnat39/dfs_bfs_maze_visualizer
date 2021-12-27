9b794c76b3c29737943dbdfa7fccd9af62bae6ce
Alex Natalenko <natalenko2001@gmail.com>
2021-12-27 13:33:19 +0300

    added drawing grid for user to draw the maze

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import DrawingPanel from "./components/maze_ui.js";
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> TITLE </h1>
+      <DrawingPanel
+          width={200}
+          height={200}
+          selectedColor={"#12345d"}
+        />
     </div>
   );
 }
--- my-app/src/models/maze.js
+++ my-app/src/models/maze.js
@@ -18,7 +18,38 @@ class Maze {
             this.grid.push(newRow);
         }
     }
+    
+    updateCell(rowNumber, columnNumber) {
+        maze[rowNumber][columnNumber].isWall = !maze[rowNumber][columnNumber].isWall;
+    }
+
 }
+
+// Box width
+var bw = 400;
+// Box height
+var bh = 400;
+// Padding
+var p = 10;
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+function drawBoard(){
+    for (var x = 0; x <= bw; x += 40) {
+        context.moveTo(0.5 + x + p, p);
+        context.lineTo(0.5 + x + p, bh + p);
+    }
+
+    for (var x = 0; x <= bh; x += 40) {
+        context.moveTo(p, 0.5 + x + p);
+        context.lineTo(bw + p, 0.5 + x + p);
+    }
+    context.strokeStyle = "black";
+    context.stroke();
+}
+
+drawBoard();
+
 //this is a test maze generation
 let maze = new Maze(3, 3);
 maze.generateMaze();
